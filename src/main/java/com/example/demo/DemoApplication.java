98cf1f8d9a3e67d4d047991837e7da2205804531
주종민 <wnwhd789@naver.com>
2024-01-19 23:56:33 +0900

    테스트 2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args)
     {
         System.out.println("지금 테스트중입니다 ");
+        System.out.println("주종민 도커 파일 생성중 ");
         SpringApplication.run(DemoApplication.class, args);
     }
 
