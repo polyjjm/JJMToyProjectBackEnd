9f69ddd7ce2e83da231a1c868267fcfabf260138
주종민 <wnwhd789@naver.com>
2025-02-24 18:30:53 +0900

    메인화면 프록시 테스트

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -14,8 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                //.allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://3.36.127.136:80")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","HEAD", "OPTIONS" , "PATCH")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
