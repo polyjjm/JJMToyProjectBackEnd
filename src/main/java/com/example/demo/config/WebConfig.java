87f5367033a8e3d4b3627e65a7a80e285b2239eb
주종민 <wnwhd789@naver.com>
2025-03-01 19:48:29 +0900

    메인화면 프록시 테스트2025/03/01 -3

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,20 +1,19 @@
 package com.example.demo.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+    //public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
 
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8020","http://localhost:8082","http://3.36.127.136:8082", "http://3.36.127.136:80" , "http://3.36.127.136:8020" , "http://localhost:80" ,"http://localhost:3000" , "http://3.36.127.136:3000")
+                //.allowedOrigins("http://localhost:8020","http://localhost:8082","http://3.36.127.136:8082", "http://3.36.127.136:80" , "http://3.36.127.136:8020" , "http://localhost:80" ,"http://localhost:3000" , "http://3.36.127.136:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
