12fad8b3c8e8d7068f22988fdad4ce35b828619d
주종민 <wnwhd789@naver.com>
2025-04-21 20:23:57 +0900

    jwt 카카오 추가

--- src/main/java/com/example/demo/config/securityConfig.java
+++ src/main/java/com/example/demo/config/securityConfig.java
@@ -61,8 +61,8 @@ public class securityConfig {
     @Bean
     public CorsConfigurationSource configurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        //configuration.setAllowedOrigins(Arrays.asList("http://3.36.127.136:80"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://3.36.127.136:80" , "http://3.36.127.136:8020" , "http://3.36.127.136:3000"));
         configuration.setAllowedMethods(Arrays.asList("*")); //모든 HTTP메서드 허용
         configuration.setAllowedHeaders(Arrays.asList("*")); //모든 헤더값 허용
         configuration.setAllowCredentials(true); //자격증명허용
